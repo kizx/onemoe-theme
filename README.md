ff4fe4f1148d20b587fa7fde1bc712204b3e03d7
kizx <kizxmoe@gmail.com>
2020-02-20 19:41:35 +0800

    更新至多盘版本
    其实是通用哒

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 ## 说明
 
-这是为 onedrive 云盘程序[OneManager](https://github.com/qkqpttgf/OneManager-php)写的一个简单的 css 主题，参考了 OneIndex 的主题样式，对应 OneManager 程序版本为[21b966b43747b60ab9fb8b62c42a361379b8b53b](https://github.com/qkqpttgf/OneManager-php/tree/21b966b43747b60ab9fb8b62c42a361379b8b53b)
+这是为 onedrive 云盘程序[OneManager](https://github.com/qkqpttgf/OneManager-php)写的一个简单的 css 主题，参考了 OneIndex 的主题样式，对应 OneManager 程序版本为[Feb 20, 2020 多盘版本](https://github.com/qkqpttgf/OneManager-php/tree/62f798d8bd0304ce5efc1eedb3e90e066c0d893d)
 
 ## 预览
 
+[在线预览](https://pan.2bboy.com)  
 ![](https://files.catbox.moe/nltur7.png)  
 ![](https://files.catbox.moe/zvatil.png)
 
--- classic.php
+++ classic.php
@@ -9,7 +9,51 @@
     <meta name="keywords" content="<?php echo $n_path;?>,<?php if ($p_path!='') echo $p_path.','; echo $_SERVER['sitename'];?>,OneManager,auth_by_逸笙">
     <link rel="icon" href="<?php echo $_SERVER['base_disk_path'];?>favicon.ico" type="image/x-icon" />
     <link rel="shortcut icon" href="<?php echo $_SERVER['base_disk_path'];?>favicon.ico" type="image/x-icon" />
-    <link rel="stylesheet" href="https://files.catbox.moe/rwt6kc.css" type="text/css">
+    <style type="text/css">
+        body{font-family:'Helvetica Neue',Helvetica,Arial,sans-serif;font-size:14px;line-height:1em;background-color:#f7f7f9;color:#000}
+        a{color:#24292e;cursor:pointer;text-decoration:none}
+        a:hover{color:#24292e}
+        .login ion-icon{vertical-align:bottom}
+        .changelanguage{position:absolute;right:5px;}
+        .title{text-align:center;margin-top:1rem;letter-spacing:2px;margin-bottom:2rem}
+        .title a{color:#333;text-decoration:none}
+        .list-wrapper{width:80%;margin:0 auto 30px;position:relative;box-shadow:0 0 32px 0 rgb(128,128,128);border-radius:15px;}
+        .list-container{position:relative;overflow:hidden;border-radius:15px;}
+        .list-header-container{position:relative}
+        .list-header-container a.back-link{color:#000;display:inline-block;position:absolute;font-size:16px;margin:20px 10px;padding:10px 10px;vertical-align:middle;text-decoration:none}
+        .list-container,.list-header-container,.list-wrapper,a.back-link:hover,body{color:#24292e}
+        .list-header-container .table-header{margin:0;border:0 none;padding:30px 60px;text-align:left;font-weight:400;color:#000;background-color:#f7f7f9;word-break: break-all;word-wrap: break-word;}
+        .list-body-container{position:relative;left:0;overflow-x:hidden;overflow-y:auto;box-sizing:border-box;background:#fff}
+        .more-disk{margin:0;border:0 none;padding:30px 30px;text-align:left;font-weight:400;color:#000;background-color:#f7f7f9;word-break: break-all;word-wrap: break-word;}
+        .more-disk a{padding:5px}
+        .list-table{width:100%;padding:0 20px 20px 20px;border-spacing:0}
+        .list-table tr{height:40px}
+        .list-table tr[data-to]:hover{background:#f1f1f1}
+        .list-table tr:first-child{background:#fff}
+        .list-table td,.list-table th{padding:0 10px;text-align:left}
+        .list-table .size,.list-table .updated_at{text-align:right}
+        .list-table .file ion-icon{font-size:15px;/*margin-right:5px;*/vertical-align:bottom}
+        .mask{position:absolute;left:0px;top:0px;width:100%;background-color:#000;filter:alpha(opacity=50);opacity:0.5;z-index:2;}
+<?php if ($_SERVER['admin']) { ?>
+        .operate{display:inline-table;margin:0;list-style:none;}
+        .operate ion-icon{vertical-align:bottom}
+        .operate ul{position:absolute;display:none;background:#fffaaa;border:0px #f7f7f7 solid;border-radius:5px;margin:-7px 0 0 0;padding:0 7px;color:#205D67;z-index:1;}
+        .operate:hover ul{position:absolute;display:inline-table;}
+        .operate ul li{padding:7px;list-style:none;display:inline-table;}
+        .operate ul li ion-icon{vertical-align:bottom}
+<?php } ?>
+        .operatediv{position:absolute;border:1px #CCCCCC;background-color:#FFFFCC;z-index:2;}
+        .operatediv div{margin:16px}
+        .operatediv_close{position:absolute;right:3px;top:3px;}
+        .readme{padding:8px;background-color:#fff;}
+        #readme{padding:20px;text-align:left}
+        @media only screen and (max-width:480px){
+            .title{margin-bottom:24px}
+            .list-wrapper{width:95%; margin-bottom:24px;}
+            .list-table {padding:8px}
+            .list-table td, .list-table th{padding:0 10px;text-align:left;white-space:nowrap;overflow:auto;max-width:80px}
+        }
+    </style>
 </head>
 
 <body>
